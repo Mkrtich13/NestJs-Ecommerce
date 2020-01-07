f54e6402214ef455762e9fb697758e053aa6989a
HOVANISIAN Mkrtich <hovanisianmkrtich@mbpdehovanisian.home>
2020-01-07 23:07:08 +0100

    now

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const PORT = process.env.PORT || 3000;
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
   await app.listen(PORT);
